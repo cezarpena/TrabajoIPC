2220eba885a614dfc8b67332015d6549d1bfec92
Petroo9 <150084027+Petroo9@users.noreply.github.com>
2026-05-18 22:34:04 +0200

    Completado de actividades ultimas faltantes de ActivityController.java

--- src/controllers/ActivityController.java
+++ src/controllers/ActivityController.java
@@ -4,20 +4,24 @@
  */
 package controllers;
 
+import java.io.File;
 import java.net.URL;
 import java.time.Duration;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import upv.ipc.sportlib.Activity;
 import upv.ipc.sportlib.SportActivityApp;
 
@@ -43,7 +47,6 @@ public class ActivityController implements Initializable {
         List<Activity> acts = app.getUserActivities();
         activityList.getItems().setAll(acts);
 
-        // Celda personalizada
         activityList.setCellFactory(lv -> new ListCell<Activity>() {
             @Override
             protected void updateItem(Activity item, boolean empty) {
@@ -62,7 +65,6 @@ public class ActivityController implements Initializable {
         btnDelete.setDisable(true);
         btnDelete.setOnAction(this::handleDelete);
 
-        // Listener selección
         activityList.getSelectionModel().selectedItemProperty()
             .addListener((obs, old, selected) -> {
                 btnDelete.setDisable(selected == null);
@@ -72,20 +74,76 @@ public class ActivityController implements Initializable {
                     mostrarEstadisticas(selected);
                 }
             });
+
+        // Cargar acumulado mensual
     }
 
     @FXML
     private void handleImport(ActionEvent event) {
-        // TODO: implementar importacion GPX
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Seleccionar fichero GPX");
+        fc.getExtensionFilters().add(
+            new FileChooser.ExtensionFilter("GPX Files", "*.gpx")
+        );
+        File file = fc.showOpenDialog(btnImport.getScene().getWindow());
+
+        if (file != null) {
+            try {
+                SportActivityApp app = SportActivityApp.getInstance();
+                Activity nueva = app.importActivity(file);
+                if (nueva != null) {
+                    activityList.getItems().add(0, nueva);
+                    activityList.getSelectionModel().select(nueva);
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Actividad importada");
+                    alert.setHeaderText(null);
+                    alert.setContentText("\"" + nueva.getName() + "\" importada correctamente.");
+                    alert.showAndWait();
+                }
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("No se pudo importar el fichero GPX: " + e.getMessage());
+                alert.showAndWait();
+            }
+        }
     }
 
     private void handleDelete(ActionEvent event) {
-        // TODO: implementar borrado con confirmacion
+        // añadir confirmacion
+        Activity selected = activityList.getSelectionModel().getSelectedItem();
+        if (selected != null) {
+            SportActivityApp app = SportActivityApp.getInstance();
+            app.removeActivity(selected);
+            activityList.getItems().remove(selected);
+            statsBox.setVisible(false);
+            statsBox.setManaged(false);
+        }
     }
 
     private void mostrarEstadisticas(Activity act) {
         lblActivityName.setText(act.getName());
         lblDistancia.setText(String.format("%.2f km", act.getTotalDistance() / 1000.0));
-        // TODO: completar resto de estadisticas
+
+        Duration dur = act.getDuration();
+        long h = dur.toHours();
+        long m = dur.toMinutesPart();
+        long s = dur.toSecondsPart();
+        lblDuracion.setText(String.format("%02d:%02d:%02d", h, m, s));
+
+        lblVelocidad.setText(String.format("%.1f km/h", act.getAverageSpeed()));
+        lblRitmo.setText(formatPace(act.getAveragePace()));
+
+        lblDesnPos.setText(String.format("+ %.0f m", act.getElevationGain()));
+        lblDesnNeg.setText(String.format("- %.0f m", act.getElevationLoss()));
+        lblAltMin.setText(String.format("%.0f m", act.getMinElevation()));
+        lblAltMax.setText(String.format("%.0f m", act.getMaxElevation()));
+    }
+
+    private String formatPace(double paceMinPerKm) {
+        int min = (int) paceMinPerKm;
+        int sec = (int) Math.round((paceMinPerKm - min) * 60);
+        return min + "'" + String.format("%02d", sec) + "\" /km";
     }
 }
